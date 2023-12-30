bbbcd2b457b36760b6766df0be0fb600c5c53fa6
Domminic Mayer <domminicm@nevada.unr.edu>
2023-12-29 22:22:32 -0800

    added mongodb functionality

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Require the necessary discord.js classes
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const dbLogin = require('./mongo-auth.js');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// connects client to mongo database
+mongoModule(client);
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
